7abf61bf9c2076274cada1be65f537019a194f81
nabeelCpp <48312424+nabeelCpp@users.noreply.github.com>
2021-11-05 17:37:53 +0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 $sub_folder = 'tech_crow';
 $site_url = $_SERVER['DOCUMENT_ROOT'].'/'.$sub_folder.'/';
-$base_url = $_SERVER['HTTP_HOST'].'/'.$sub_folder.'/';
+$base_url = "http://".$_SERVER['HTTP_HOST'].'/'.$sub_folder.'/';
